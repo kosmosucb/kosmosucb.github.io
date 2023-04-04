2a739dfd6bd8a3cc37ad8fb0549bf75ab706df25
kosmosucb <129908448+kosmosucb@users.noreply.github.com>
2023-04-04 16:52:04 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,4 @@
+// Toggle the navigation menu when the button is clicked
+document.querySelector('.nav-toggle').addEventListener('click', function() {
+  document.querySelector('.nav-menu').classList.toggle('nav-menu--active');
+});
